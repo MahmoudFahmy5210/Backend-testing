cb227cf724671692c76dca49d1797829966ad0a2
Mahmoud samir <msfa515@gmail.com>
2021-11-13 00:13:45 +0200

    from start

---
+++ index.js
@@ -0,0 +1,18 @@
+import  express  from "express";
+//= const express = require("express")
+import bodyParser from "body-parser";
+
+const app = express();
+
+const PORT = 5000;
+
+app.use(bodyParser.json());//this means we will use data in our app in json format
+
+//"/" means home page 
+// note : when you go to google you do (get) 
+app.get('/',(req,res)=>{
+    res.send("Hello from Home page ya 7oka");
+}) 
+
+app.listen(PORT,()=>{console.log(`Server is Running on port : http://localhost:${PORT}`)
+});
